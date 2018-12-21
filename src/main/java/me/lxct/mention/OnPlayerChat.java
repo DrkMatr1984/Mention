f7a75668358ef7f8f81079bfd5513d0d7c03110d
Lxct <sofiane.djerbi@yahoo.com>
2018-12-21 16:38:42 +0100

    Fixed few things

--- src/main/java/me/lxct/mention/OnPlayerChat.java
+++ src/main/java/me/lxct/mention/OnPlayerChat.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
@@ -13,12 +14,13 @@ import static me.lxct.mention.Functions.formatMsg;
 import static me.lxct.mention.Variable.*;
 
 public class OnPlayerChat implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public static void onPlayerChat(AsyncPlayerChatEvent event) {
-        String message = event.getMessage().toUpperCase();
+        String msg = event.getMessage().toUpperCase();
+        String message = event.getMessage();
         vPlayer = event.getPlayer();
         for (Player player : Bukkit.getOnlinePlayers()) {
-            if (message.contains(player.getName().toUpperCase()) && !player.hasPermission("mention.ignore")) {
+            if (msg.contains(player.getName().toUpperCase()) && !player.hasPermission("mention.ignore")) {
                 vTarget = player;
                 if (useChat) {
                     player.sendMessage(formatMsg(chatMsg));
@@ -29,14 +31,14 @@ public class OnPlayerChat implements Listener {
                 if (playSound) {
                     player.playSound(player.getLocation(), sound, soundVolume, soundPitch);
                 }
-                if (message.contains(symbol + player.getName().toUpperCase())) {
-                    event.setMessage(event.getMessage().replaceAll(symbol + "(?i)" + player.getName(), colorize(color + symbol + player.getName() + "&r")));
-                } else if (message.contains(player.getName().toUpperCase())) {
-                    event.setMessage(event.getMessage().replaceAll("(?i)" + player.getName(), colorize(color + symbol + player.getName() + "&r")));
+                if (msg.contains(symbol + player.getName().toUpperCase())) {
+                    message = message.replaceAll(symbol + "(?i)" + player.getName(), colorize(color + symbol + player.getName() + "&r"));
+                } else if (msg.contains(player.getName().toUpperCase())) {
+                    message = message.replaceAll("(?i)" + player.getName(), colorize(color + symbol + player.getName() + "&r"));
                 }
             }
         }
-        if (message.contains(symbol + "EVERYONE") && event.getPlayer().hasPermission("mention.everyone")) {
+        if (msg.contains(symbol + "EVERYONE") && event.getPlayer().hasPermission("mention.everyone")) {
             for (Player player : Bukkit.getOnlinePlayers()) {
                 if (useChat) {
                     player.sendMessage(formatMsg(chatMsg));
@@ -48,8 +50,9 @@ public class OnPlayerChat implements Listener {
                     player.playSound(player.getLocation(), sound, soundVolume, soundPitch);
                 }
             }
-            event.setMessage(event.getMessage().replaceAll(symbol + "(?i)everyone", colorize(color + symbol + "everyone" + "&r")));
-        } else if (message.contains(symbol + "HERE") && event.getPlayer().hasPermission("mention.everyone")) {
+            message = message.replaceAll(symbol + "(?i)everyone", colorize(color + symbol + "everyone" + "&r"));
+        }
+        if (msg.contains(symbol + "HERE") && event.getPlayer().hasPermission("mention.everyone")) {
             for (Player player : Bukkit.getOnlinePlayers()) {
                 if (useChat) {
                     player.sendMessage(formatMsg(chatMsg));
@@ -61,7 +64,8 @@ public class OnPlayerChat implements Listener {
                     player.playSound(player.getLocation(), sound, soundVolume, soundPitch);
                 }
             }
-            event.setMessage(event.getMessage().replaceAll(symbol + "(?i)here", colorize(color + symbol + "here" + "&r")));
+            message = message.replaceAll(symbol + "(?i)here", colorize(color + symbol + "here" + "&r"));
         }
+        event.setMessage(message);
     }
 }
